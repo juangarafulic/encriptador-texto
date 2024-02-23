218685ee8cabf88885eebfbac123964ace4a6bcb
juangarafulic <jgarafulic@hotmail.com>
2024-02-22 23:51:06 -0300

    Crea función para boton encriptar

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-let textoSalida = document.getElementById("texto-salida").style.display = "none";
+let textoEntrada = document.querySelector("#texto-entrada"); //colocar # es clave
+let textoSalida = document.querySelector("#texto-salida");
+let setReglas = [
+    ["a", "ai"], // 0
+    ["e", "enter"], // 1
+    ["i", "imes"], // 2
+    ["o", "ober"], // 3
+    ["u", "ufat"] // 4
+];
+
+function botonEncriptar() {
+    let texto = textoEntrada.value;
+    console.log(texto);
+}
+
+/*
+if (texto == null) {
+document.getElementById("texto-salida").style.display = "none";
+} else {
+    document.getElementById("persona").style.display = "none";
+}
+*/
